cfc636f2d44f5cc88dd719d2c204a69f0dcd4060
Weskley Bezerra <weskleymb@hotmail.com>
2023-07-10 13:57:51 -0300

    funcoes

--- javascript/alfabeto.js
+++ javascript/alfabeto.js
@@ -3,17 +3,18 @@ let letra = prompt(`Digite`).toLowerCase();
 let ehLetraDoAlfabeto = letra.charCodeAt(0) >= 97 && letra.charCodeAt(0) <= 122;
 
 if (ehLetraDoAlfabeto) {
-    let ehVogal = letra.charCodeAt(0) == 97 
-                || letra.charCodeAt(0) == 101 
-                || letra.charCodeAt(0) == 111
-                || letra.charCodeAt(0) == 105
-                || letra.charCodeAt(0) == 117;
+  let ehVogal =
+    letra.charCodeAt(0) == 97 ||
+    letra.charCodeAt(0) == 101 ||
+    letra.charCodeAt(0) == 111 ||
+    letra.charCodeAt(0) == 105 ||
+    letra.charCodeAt(0) == 117;
 
-    if (ehVogal) {
-        alert(`A letra é uma vogal`);
-    } else {
-        alert(`A letra é uma consoante`)
-    }
+  if (ehVogal) {
+    alert(`A letra é uma vogal`);
+  } else {
+    alert(`A letra é uma consoante`);
+  }
 } else {
-    alert(`Não é uma letra do alfabeto`);
+  alert(`Não é uma letra do alfabeto`);
 }
