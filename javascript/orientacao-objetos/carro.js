241c2400c510b49deade8a5e06893186d1ad0cdd
Weskley Bezerra <weskleymb@hotmail.com>
2023-07-17 19:53:22 -0300

    orientacao a objetos

---
+++ javascript/orientacao-objetos/carro.js
@@ -0,0 +1,19 @@
+export default class Carro {
+
+    // atributos
+    marca;
+    modelo;
+    ano;
+
+    // construtor
+    constructor(modelo, marca, ano) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+    }
+
+    // metodos
+
+}
+
+
---
+++ javascript/orientacao-objetos/main.js
@@ -0,0 +1,9 @@
+import { Carro } from './carro.js';
+
+// instanciando o objeto up
+const up = new Carro(`Up`, `VW`, 2015);
+
+// instanciando o objeto ka
+const ka = new Carro(`Ka`, `Ford`, 2006);
+
+alert(up.ano);
